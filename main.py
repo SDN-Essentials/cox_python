cd88671c138f5958143092ab79c8702165369e19
darien-hirotsu <darien@sdnessentials.com>
2018-11-06 13:25:32 -0500

    Updates

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
 #!/usr/bin/env python
 
 
-from net_device import EX
-from net_device import SRX
+from EX import EX
+from SRX import SRX
 import sys
 
 
 def main():
     srx_1 = SRX('172.16.0.1', 'netconf', 'test123')
     srx_1.connect()
-    print(srx_1.routes())
     ex_1 = EX('172.16.0.1', 'netconf', 'test123')
     ex_1.connect()
-    print(ex_1.routes())
+    # Needs to print the message below.
     if ex_1.routes() == srx_1.routes():
         print('Eeeexcellent.')
 
